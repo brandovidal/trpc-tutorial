ccb39ae8fd309589413be106b5d55e7d0872ffc0
Brando Vidal <yisusdracon@gmail.com>
2023-01-13 23:43:30 -0500

    ⚒  configure server with express
    Signed-off-by: Brando Vidal <yisusdracon@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,28 @@
 import express from 'express'
+import cors from 'cors'
+
+import { initTRPC } from '@trpc/server'
+import * as trpcExpress from '@trpc/server/adapters/express'
 
 const app = express()
+const trpc = initTRPC.create()
+
+const router = trpc.router
+const publicProcedure = trpc.procedure
+
+const appRouter = router({
+  hello: publicProcedure.query(() => 'hello world dev')
+})
+
+// export type AppRouter = typeof appRouter
+
+app.use(cors())
+app.use(
+  '/trpc',
+  trpcExpress.createExpressMiddleware({
+    router: appRouter
+  })
+)
 
 app.listen(3000, () => {
   console.log('listening in port 3000')
